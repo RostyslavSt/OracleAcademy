7f84c7977e10fed981f4b2edccd5f272f1331106
rost <rost@ukr.net>
2017-03-28 11:35:44 +0300

    add description

--- mon03_20_2017_ES6_2_classes/task5/main.js
+++ mon03_20_2017_ES6_2_classes/task5/main.js
@@ -6,6 +6,9 @@ class Round {
         this.pointY = y;
         this.radius = radius;
     }
+
+    // 3. Определите статический метод, который принимает радиус и возвращает
+    // длину окружности для заданного радиуса (L = 2 * π * R);
     static getLengthСircle(radius) {
         return 2 * 3.14 * radius;
     }
@@ -16,10 +19,12 @@ class Round {
         return new Round(x, y, radius);
     }
 
+    // 2. Определите метод получения длины окружности для текущего объекта (L = 2* π * R);
     getLengthСircle() {
         return 2 * 3.14 * this.radius;
     }
 
+    // 6. Определите метод проверки попадания точки в круг;
     // (x-x1)^2+(y-y1)^2<R^2, где x1,y1 координаты центра круга
     enterPoint(x, y) {
         let x1 = x - this.pointX;
@@ -29,16 +34,17 @@ class Round {
         } else return false;
     }
 
+    // 4. Определите метод получения объекта-круга, который возвращает копию текущего объекта;
     makeCopy() {
         return new Round(this.pointX, this.pointY, this.radius)
     }
+    // 7. Определите метод преобразования текущего состояния объекта всимвольную строку (toString()).
     toString() {
         for (var key in this) {
             console.log(`The key: '${key}'. And it is value: '${this[key]}'`);
         }
     }
 }
-
 
 let round = new Round(3, 4, 15);
 let copyRound = round.makeCopy();
